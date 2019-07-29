1b5aa00d5e02769e45262ccf5ad01d1c6800bc85
erikburt <eb.erik.burton@gmail.com>
2019-07-29 13:31:38 -0400

    bpm slider implementation

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,13 @@
-import React from "react";
-import { Component } from "react";
-import SequencerContainer from "./components/sequencerContainer";
-import "./css/App.css";
+import React from 'react';
+import { Component } from 'react';
+import SequencerContainer from './components/sequencerContainer';
+import './css/App.css';
 
 export interface AppProps {}
 
 export interface AppState {
   audioContext: AudioContext;
+  bpm: number;
 }
 
 class App extends React.Component<AppProps, AppState> {
@@ -14,18 +15,40 @@ class App extends React.Component<AppProps, AppState> {
     super(props);
 
     this.state = {
+      bpm: 120,
       audioContext: new AudioContext()
     };
+
+    this.bpmChange = this.bpmChange.bind(this);
   }
+
+  bpmChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const bpm = parseInt(e.currentTarget.value) || 120;
+    this.setState({ bpm });
+  };
 
   render() {
     return (
-      <SequencerContainer
-        key="SequencerContainer"
-        bpm={120}
-        numSequencers={15}
-        audioContext={this.state.audioContext}
-      />
+      <div>
+        <div>
+          {this.state.bpm + 'bpm'}
+          <input
+            className="bpmslider"
+            type="range"
+            min="30"
+            max="600"
+            defaultValue="120"
+            onChange={this.bpmChange}
+          />
+        </div>
+
+        <SequencerContainer
+          key="SequencerContainer"
+          bpm={this.state.bpm}
+          numSequencers={15}
+          audioContext={this.state.audioContext}
+        />
+      </div>
     );
   }
 }
--- src/components/sequencerContainer.tsx
+++ src/components/sequencerContainer.tsx
@@ -1,24 +1,25 @@
-import * as React from "react";
-import { Component } from "react";
-import Sequencer from "./sequencer";
-import AudioBufferWrapper from "../tools/audioBufferWrapper";
+import * as React from 'react';
+import { Component } from 'react';
+import Sequencer from './sequencer';
+import AudioBufferWrapper from '../tools/audioBufferWrapper';
+import { tsImportEqualsDeclaration } from '@babel/types';
 
 const AUDIO_ARRAY = [
-  "clap01.wav",
-  "clap02.wav",
-  "closedhat01.wav",
-  "closedhat02.wav",
-  "drop.wav",
-  "kick01.wav",
-  "kick02.wav",
-  "laser.wav",
-  "openhat01.wav",
-  "openhat02.wav",
-  "snare01.wav",
-  "snare02.wav",
-  "tom01.wav",
-  "tom02.wav",
-  "tom03.wav"
+  'clap01.wav',
+  'clap02.wav',
+  'closedhat01.wav',
+  'closedhat02.wav',
+  'drop.wav',
+  'kick01.wav',
+  'kick02.wav',
+  'laser.wav',
+  'openhat01.wav',
+  'openhat02.wav',
+  'snare01.wav',
+  'snare02.wav',
+  'tom01.wav',
+  'tom02.wav',
+  'tom03.wav'
 ];
 
 export interface SequencerContainerProps {
@@ -31,6 +32,7 @@ export interface SequencerContainerState {
   step: number;
   interval: number;
   audioRefs: AudioBufferWrapper[];
+  bpm: number;
 }
 
 class SequencerContainer extends React.Component<
@@ -52,8 +54,11 @@ class SequencerContainer extends React.Component<
     this.state = {
       step: 0,
       interval: Math.ceil((60 * 1000) / bpm),
-      audioRefs
+      audioRefs,
+      bpm
     };
+
+    // this.handleDrop = this.handleDrop.bind(this);
   }
 
   tick() {
@@ -74,13 +79,28 @@ class SequencerContainer extends React.Component<
     clearInterval(this.timer);
   }
 
+  updateBPM = () => {
+    const { bpm } = this.props;
+    this.setState({ bpm, interval: Math.ceil((60 * 1000) / bpm) });
+
+    clearInterval(this.timer);
+
+    this.timer = window.setInterval(() => {
+      this.tick();
+    }, this.state.interval);
+  };
+
   render() {
-    let test: JSX.Element[] = [];
+    if (this.props.bpm !== this.state.bpm) {
+      this.updateBPM();
+    }
+
+    let sequencers: JSX.Element[] = [];
 
     for (let i = 0; i < this.props.numSequencers; i++) {
       const audio = this.state.audioRefs[i % this.state.audioRefs.length];
 
-      test.push(
+      sequencers.push(
         <Sequencer
           key={i}
           id={i}
@@ -92,7 +112,7 @@ class SequencerContainer extends React.Component<
       );
     }
 
-    return test;
+    return sequencers;
   }
 }
 
