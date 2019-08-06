3700faaeb13c936bb9968904b8b5876ffc83b283
Erik <eb.erik.burton@gmail.com>
2019-08-06 15:48:16 -0400

    Adding build files for static github hosting

---
+++ build/precache-manifest.c8de141df1b6db172848b222ee695a05.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b86de6df8f16536f388d2f8d4a64f010",
+    "url": "/index.html"
+  },
+  {
+    "revision": "537b0ec0f6c5fe9c404d",
+    "url": "/static/css/main.a3c2e0a1.chunk.css"
+  },
+  {
+    "revision": "2884799180ec41c3b856",
+    "url": "/static/js/2.c9e9099d.chunk.js"
+  },
+  {
+    "revision": "537b0ec0f6c5fe9c404d",
+    "url": "/static/js/main.b21c08aa.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
